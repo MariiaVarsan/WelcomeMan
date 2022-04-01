aa370bd487593ed6bdb27ff0beb5fc6d52848b67
Mariia <mariiavarsan@gmail.com>
2022-04-01 17:05:13 +0300

    Initial Commit

---
+++ WelcomeMan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WelcomeMan
+//
+//  Created by Варсан Мария on 01.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
